d5de0a68c744f5375272e82fcf6e5caba11eb2dd
alam.alfaris <alam.alfaris@whiteopen.com>
2022-10-12 17:32:29 +0700

    Add project files.

---
+++ BlazorChatSignalR/Server/Program.cs
@@ -0,0 +1,52 @@
+using BlazorChatSignalR.Server.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+//for SignalR [start]
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(options =>
+    options.MimeTypes = ResponseCompressionDefaults
+    .MimeTypes
+    .Concat(new[] { "application/octet-stream" }));
+//for SignalR [end]
+
+var app = builder.Build();
+
+//for SignalR [start]
+app.UseResponseCompression();
+//for SignalR [end]
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+//for SignalR [start]
+app.MapHub<ChatHub>("/chathub");
+//for SignalR [end]
+app.MapFallbackToFile("index.html");
+
+app.Run();
